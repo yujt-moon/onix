fd30e998257760c1433d0e7bc994804400a8b565
yujt-moon <1291932524@qq.com>
2023-05-17 20:23:02 +0800

    027 全局描述符表

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -6,14 +6,13 @@
 #include <onix/printk.h>
 #include <onix/assert.h>
 #include <onix/debug.h>
+#include <onix/global.h>
 
 void kernel_init()
 {
     console_init();
     
-    BMB;
-
-    DEBUGK("debug onix!!!\n");
+    gdt_init();
 
     return;
 }
