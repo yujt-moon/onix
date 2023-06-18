203a6e6011b1574c0bdccc06a62dc9b11fe62e32
yujt-moon <1291932524@qq.com>
2023-06-18 22:53:30 +0800

    032 外中断原理

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -9,6 +9,7 @@
 #include <onix/global.h>
 #include <onix/task.h>
 #include <onix/interrupt.h>
+#include <onix/stdlib.h>
 
 void kernel_init()
 {
@@ -19,6 +20,18 @@ void kernel_init()
     interrupt_init();
 
     // task_init();
+
+    asm volatile(
+        "sti\n"
+        "movl %eax, %eax\n");
+
+    u32 counter = 0;
+    while (true)
+    {
+        DEBUGK("looping in kernel init %d...\n", counter++);
+        delay(100000000);
+    }
+    
 
     return;
 }
