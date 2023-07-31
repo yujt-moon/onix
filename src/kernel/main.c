7be3ffe01c7dc8f29e9a7f5582b066d988876e68
yujt-moon <1291932524@qq.com>
2023-07-31 22:22:41 +0800

    037 时间

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,16 +2,18 @@ extern void console_init();
 extern void gdt_init();
 extern void interrupt_init();
 extern void clock_init();
+extern void time_init();
 extern void hang();
 
 void kernel_init()
 {
     console_init();
     gdt_init();
-    interrupt_init();
-    // task_init();
+    interrupt_init();  
     clock_init();
+    time_init();
 
+    // task_init();
     asm volatile("sti");
     hang();
 }
